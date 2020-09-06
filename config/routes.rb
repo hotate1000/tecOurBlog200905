2035db3d40b4ee4db4a93a926fe6086963e3749c
hotate1000 <ooo.336.sss@gmail.com>
2020-09-06 14:37:01 +0900

    ユーザー詳細ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
   # resources :posts, except: [:index]
+  resources :users, only: [:show]
 end
